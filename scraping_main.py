eb691adb4cbf479271b78347ada6e5f250007c70
pydeveloper510 <85598258+pydeveloper510@users.noreply.github.com>
2021-06-17 11:10:50 +0800

    Add files via upload

---
+++ scraping_main.py
@@ -0,0 +1,34 @@
+# ####################   SCRAPING MAIN FLOW   ####################
+# ####################   SCRIPT FEATURES   ####################
+# 1.
+# 2.
+# 3.
+
+# ####################   IMPORT LIBRARIES   ####################
+import os
+# ...
+
+# ####################   DEFINE SETTINGS   ####################
+# from ... import ... as ...
+# file manager read write append cav xlsx json yaml ini (config.ini, config.yaml)
+# requests
+# multithreading
+# multiprocess
+# async
+# db manager
+# GUI pyqt5 tkinter pygame
+# ...
+
+# ####################   MAIN PROCESS   ####################
+# startScraping()
+# login()
+# ...
+
+# ####################   END SCRAPING   ####################
+
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
