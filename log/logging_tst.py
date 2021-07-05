1dc5f3230db5cc2c4ed615c403de76a3549c40ba
pydeveloper510 <85598258+pydeveloper510@users.noreply.github.com>
2021-07-06 01:07:14 +0800

    Update logging_tst.py

--- log/logging_tst.py
+++ log/logging_tst.py
@@ -3,3 +3,9 @@ logging.basicConfig(filename='example.log',level=logging.DEBUG)
 logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('And this, too')
+
+===========================================================================
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+logging.warning('Total time the function({}) was executed: {} minutes {} seconds'.format(func.__name__, minutes, seconds))
